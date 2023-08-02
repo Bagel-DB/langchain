779887dc639ab9f73dba0a9bae18e904c5a21077
Towhid1 <nurulaktertowhid@gmail.com>
2023-08-02 17:35:46 +0600

    test_add_only_texts

--- libs/langchain/test_bagel.py
+++ libs/langchain/test_bagel.py
@@ -1,13 +1,25 @@
 from langchain.vectorstores import Bagel
 from bagel.config import Settings
 
-def test_create_bagel() -> None:
+
+def test_create_bagel() -> Bagel:
     setting = Settings(
         bagel_api_impl="rest",
         bagel_server_host="api.bageldb.ai",
     )
-    bagel_obj = Bagel(client_settings=setting)
-    # print("ping :", bagel_obj._client.ping())
+    return Bagel(client_settings=setting)
 
 
-test_create_bagel()
+def test_add_only_texts(bagel: Bagel) -> None:
+    bagel.add_texts(texts=["hello bagel", "hello langchain"])
+    print(">> add_texts with only text")
+
+
+def main():
+    """Bagel intigaration test"""
+    bagel_instance = test_create_bagel()
+    test_add_only_texts(bagel_instance)
+
+
+if __name__ == "__main__":
+    main()
